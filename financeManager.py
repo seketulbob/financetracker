4fdc2579e2e70ebc157ea2cc63b92a449c8589c1
bob <c2064179@newcastle.ac.uk>
2023-10-31 12:23:17 +0000

    Added file object
    Added csv_reader object to read csv file and print the content

---
+++ financeManager.py
@@ -0,0 +1,11 @@
+import csv
+
+MONTH = 'sept'
+
+file = f"lloyds_{MONTH}23.csv"
+
+with open(file, mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        print(row)
+
